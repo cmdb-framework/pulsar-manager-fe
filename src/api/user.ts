512e7bb4ff12b2916bf7f8cb328e2fd1f8956c26
NachuanGu <gncgnc97@gmail.com>
2024-02-20 01:40:36 +0800

    同步了部分代码

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,7 +2,7 @@ import Api from '@/api/baseRequest'
 import type { LoginSchema } from '@/views/login/schema'
 
 const userLogin = (data: LoginSchema) => {
-  return Api.post('/user/login/', data)
+  return Api.post('/user/login', data)
 }
 
 export {userLogin}
